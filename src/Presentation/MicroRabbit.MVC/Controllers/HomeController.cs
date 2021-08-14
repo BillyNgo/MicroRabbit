17f66f8a5fc7f08ce232b4d9114da5340682c1dc
Billy Ngo <thanhdat.hitech@gmail.com>
2021-08-14 18:37:16 +0700

    Add Presentation: MVC Application

---
+++ src/Presentation/MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Services;
+using MicroRabbit.MVC.Models.DTO;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    /**
+     * Home api controller
+     * 
+     * @author D. P. Edwards
+     * @license MIT
+     * @version 1.0
+     */ 
+    public class HomeController : Controller
+    {
+        private readonly ITransferService _transferService;
+
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDto transferDto = new TransferDto()
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDto);
+
+            return View("Index");
+        }
+    }
+}
---
+++ src/Presentation/MicroRabbit.MVC/Models/TransferViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MicroRabbit.MVC.Models
+{
+
+    /**
+     * Transfer View Model class
+     * 
+     * This class holds some notes and logs.
+     * 
+     * @author D. P. Edwards
+     * @license MIT
+     * @version 1.0
+     */
+    public class TransferViewModel
+    {
+        public string TransferNotes { get; set; }
+        public int FromAccount { get; set; }
+        public int ToAccount { get; internal set; }
+        public decimal TransferAmount { get; set; }
+    }
+}
